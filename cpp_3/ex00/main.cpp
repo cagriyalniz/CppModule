c69d389ed1c216b022e4d0de0463991a74a0761d
cagriyalniz <cagriyalniz@gmail.com>
2022-09-28 00:41:45 +0300

    not completed

--- cpp_3/ex00/main.cpp
+++ cpp_3/ex00/main.cpp
@@ -4,5 +4,13 @@ int main()
 {
     ClapTrap fighter("cagri");
     fighter.attack("XxX");
+    fighter.setAttackDamage(3);
+    std::cout<<fighter.getEnergyPoints()<<std::endl;
+    while(fighter.getEnergyPoints() >= 0){
+        fighter.attack("XxX");
+        if(fighter.getEnergyPoints() == 0)
+            break ;
+    }
+    fighter.takeDamage(1000);
 
 }
